447171fa1ec6e07c9175b66ff793495440540a10
Deanna PIEDRA <dpiedra@student.42.fr>
2020-02-03 16:59:25 +0100

    try

---
+++ ft_itoa_base.c
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa_base.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dpiedra <dpiedra@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/03 16:22:02 by dpiedra           #+#    #+#             */
+/*   Updated: 2020/02/03 16:58:58 by dpiedra          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "printf.h"
+
+static int	num_length(unsigned int n, int b)
+{
+	int	i;
+
+	i = 0;
+	while (n > 0)
+	{
+		n /= b;
+		i++;
+	}
+	return (i);
+}
+
+int			putnbr(unsigned int nb, char *base, char *str, int l)
+{
+	int	b;
+
+	b = ft_strlen(base);
+	if (l == 0)
+	{
+		str[l] = base[nb % b - 1];
+		return (0);
+	}
+	else
+	{
+		str[l] = base[nb % b];
+		return (putnbr(nb / b, base, str, --l));
+	}
+}
+
+char		*ft_itoa_base(unsigned int nbr, char *base)
+{
+	int		b;
+	char	*str;
+	int		l;
+
+	b = ft_strlen(base);
+	l = num_length(nbr, b);
+	if (!(str = malloc(sizeof(char) * l + 1)))
+		return (0);
+	str[l] = '\0';
+	putnbr(nbr, base, str, l - 1);
+	return (str);
+}
+
+int			main(void)
+{
+	int i;
+
+	i = 0;
+	printf("%s\n", ft_itoa_base(i, "123456789abcdef"));
+	printf("%x\n", i);
+	return (0);
+}
