a8aba93bf82ae043a31dc1749f24f7587d3383f2
Deanna PIEDRA <dpiedra@student.42.fr>
2020-02-06 17:27:48 +0100

    fixed norme errors

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: dpiedra <dpiedra@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/16 17:16:20 by dpiedra           #+#    #+#             */
-/*   Updated: 2020/02/06 17:03:24 by dpiedra          ###   ########.fr       */
+/*   Updated: 2020/02/06 17:21:53 by dpiedra          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,14 @@
 
 int	main(void)
 {
-	char			c;
 	char			*str;
-	char			*str2;
-	int				deci;
-	unsigned int	unsi;
-	int				i;
-	char d;
 	int				a;
 	int				b;
 
 	str = "test printf";
-	str2 = "hello printf";
-	unsi = -21;
-	c = 'd';
-	d = 'a';
-	i = -12;
-	deci = -52;
 	printf("CONVERSION D'ENTIER NATUREL (PTR - p)\n");
 	a = ft_printf("|1 : %p|\n", str);
-	b =    printf("|1 : %p|\n", str);
+	b = printf("|1 : %p|\n", str);
 	printf(" Your printf returned : %i\nTheir printf returned : %i\n", a, b);
 	return (0);
 }
--- printf.h
+++ printf.h
@@ -6,7 +6,7 @@
 /*   By: dpiedra <dpiedra@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/16 17:16:44 by dpiedra           #+#    #+#             */
-/*   Updated: 2020/02/06 17:09:01 by dpiedra          ###   ########.fr       */
+/*   Updated: 2020/02/06 17:26:24 by dpiedra          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,52 +20,52 @@
 
 typedef struct	s_flag
 {
-	int	zero;
-	int	minus;
-	int	width;
-	int period;
-	int	precision;
+	int			zero;
+	int			minus;
+	int			width;
+	int			period;
+	int			precision;
 }				t_flag;
 
-int		ft_printf(const char *, ...);
-int		ft_new_putchar(char c, int rtn);
-t_flag	ft_start_flags(t_flag flags);
-t_flag	ft_make_flags(const char *str, t_flag flags, va_list args);
-int		ft_isflag(char c);
-t_flag	ft_sort_flag(char c, t_flag flags, va_list args);
-int		ft_isdigit(int c);
-t_flag	ft_arg_flags(char c, t_flag flags, va_list args);
-int		ft_find_conv(const char *str, va_list args, t_flag flags);
-int		ft_c_conv(va_list args, t_flag flags);
-void	ft_putchar_fd(char c, int fd);
-int		ft_fix_field(int flag, char c);
-int		ft_str_conv(va_list args, t_flag flags);
-size_t	ft_strlen(const char *str);
-int		ft_str_width(t_flag flags, char *str, int count);
-int		ft_str_precision(t_flag flags, char *str, int count);
-int		ft_neg_precision(t_flag flags, char *str, int count);
-void	ft_putstr_fd(char *s, int fd);
-int		ft_signed_conv(va_list args, t_flag flags);
-int		ft_find_count(int decimal);
-int		ft_signed_flags(t_flag flags, int decimal, int sign, int count);
-int		ft_signed_precision(t_flag flags, int sign, int count);
-int		ft_signed_width(t_flag flags, int sign, int count);
-void	ft_putnbr_fd(int n, int fd);
-int		ft_unsigned_conv(va_list args, t_flag flags);
-int		ft_find_ucount(unsigned int nbr);
-int		ft_unsigned_width(t_flag flags, int count);
-void	ft_putunbr_fd(unsigned int n, int fd);
-int		ft_unsigned_precision(t_flag flags, int count);
-int		ft_unsigned_flags(t_flag flags, unsigned int nbr, int count);
-int		ft_hexa_conv(va_list args, t_flag flags, int x);
-char	*ft_uitoa_base(unsigned int nbr, int base, int x);
-int		ft_hexa_flags(t_flag flags, char *str, int len);
-int		ft_hexa_precision(t_flag flags, int len);
-int		ft_hexa_width(t_flag flags, int len);
-int		ft_ptr_conv(va_list args, t_flag flags);
-char	*ft_longitoa_base(unsigned long long nbr, int base);
-int		ft_ptr_flags(t_flag flags, char *str, int len);
-int		ft_percent_conv(t_flag flags);
-int		ft_percent_flags(t_flag flags);
+int				ft_printf(const char *str, ...);
+int				ft_new_putchar(char c, int rtn);
+t_flag			ft_start_flags(t_flag flags);
+t_flag			ft_make_flags(const char *str, t_flag flags, va_list args);
+int				ft_isflag(char c);
+t_flag			ft_sort_flag(char c, t_flag flags, va_list args);
+int				ft_isdigit(int c);
+t_flag			ft_arg_flags(char c, t_flag flags, va_list args);
+int				ft_find_conv(const char *str, va_list args, t_flag flags);
+int				ft_c_conv(va_list args, t_flag flags);
+void			ft_putchar_fd(char c, int fd);
+int				ft_fix_field(int flag, char c);
+int				ft_str_conv(va_list args, t_flag flags);
+size_t			ft_strlen(const char *str);
+int				ft_str_width(t_flag flags, char *str, int count);
+int				ft_str_precision(t_flag flags, char *str, int count);
+int				ft_neg_precision(t_flag flags, char *str, int count);
+void			ft_putstr_fd(char *s, int fd);
+int				ft_signed_conv(va_list args, t_flag flags);
+int				ft_find_count(int decimal);
+int				ft_signed_flags(t_flag flags, int decimal, int sign, int count);
+int				ft_signed_precision(t_flag flags, int sign, int count);
+int				ft_signed_width(t_flag flags, int sign, int count);
+void			ft_putnbr_fd(int n, int fd);
+int				ft_unsigned_conv(va_list args, t_flag flags);
+int				ft_find_ucount(unsigned int nbr);
+int				ft_unsigned_width(t_flag flags, int count);
+void			ft_putunbr_fd(unsigned int n, int fd);
+int				ft_unsigned_precision(t_flag flags, int count);
+int				ft_unsigned_flags(t_flag flags, unsigned int nbr, int count);
+int				ft_hexa_conv(va_list args, t_flag flags, int x);
+char			*ft_uitoa_base(unsigned int nbr, int base, int x);
+int				ft_hexa_flags(t_flag flags, char *str, int len);
+int				ft_hexa_precision(t_flag flags, int len);
+int				ft_hexa_width(t_flag flags, int len);
+int				ft_ptr_conv(va_list args, t_flag flags);
+char			*ft_longitoa_base(unsigned long long nbr, int base);
+int				ft_ptr_flags(t_flag flags, char *str, int len);
+int				ft_percent_conv(t_flag flags);
+int				ft_percent_flags(t_flag flags);
 
 #endif
